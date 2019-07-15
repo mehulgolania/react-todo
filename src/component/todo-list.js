538ceb6d6201c85e735f4372ef1127761674bc81
mehul_golania <mehul.golania@vshift.com>
2019-07-15 19:06:55 +0530

    Todo list updated

--- src/component/todo-list.js
+++ src/component/todo-list.js
@@ -6,7 +6,7 @@ class TodoList extends React.Component {
   state = {
     items : [
       { title: 'buy milk', status: false },
-      { title: 'buy milk', status: false },
+      { title: 'buy bread', status: true },
     ]
   }
 
@@ -15,10 +15,22 @@ class TodoList extends React.Component {
       <>
         <h5 className="text-primary mt-5 mb-3">Todo List</h5>
         <ListGroup>
-          <ListGroup.Item className="d-flex justify-content-between">
-            <div>Text</div> 
-            <div><Badge pill variant="success">Edit</Badge> <Badge pill variant="danger">Delete</Badge></div>
-          </ListGroup.Item>
+          { this.state.items &&
+            this.state.items.map((item, key) => (
+              <ListGroup.Item className="d-flex justify-content-between text-capitalize" key={key}>
+                <div><span className={item.status ? 'completed' : 'pending'}>{item.title}</span></div>
+                <div>
+                  { item.status ? <Badge pill variant="danger" className="span-btn">Delete</Badge> : 
+                    <>
+                      <Badge pill variant="success" className="mr-2 span-btn">Edit</Badge> 
+                      <Badge pill variant="info" className="span-btn">Complete</Badge>
+                    </>
+                  } 
+                  
+                </div>
+              </ListGroup.Item>
+            ))
+          }
         </ListGroup>
 
         <AddTodo />
